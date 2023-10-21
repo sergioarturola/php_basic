8f092aa50fa6c68d1d0ba844af5b360748e3ce33
Sergio Arturo <119349223+sergioarturola@users.noreply.github.com>
2023-10-21 13:07:26 -0600

    bucles con php for-while-do_while

---
+++ bulces.php
@@ -0,0 +1,31 @@
+<?php
+//el bucle for es como en los demas lenguajes
+
+for($x = 0; $x<10; $x++){
+
+    echo "FOR ".$x."<br>";
+}
+
+echo '<br>';
+
+//ciclo while
+
+$var = 0;
+
+while($var <=12){
+
+    echo "WHILE ".$var."<br>";
+    $var++;
+}
+
+echo '<br>';
+//ciclo do-while
+$numero = 6;
+
+do{
+    echo "DO-WHILE ".$numero."<br>";
+    $numero++;
+
+}while($numero <= 5);
+
+?>
