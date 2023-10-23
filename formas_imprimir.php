ba9af0b81fcb21b4ad41e03af836d5df21bc5820
Sergio Arturo <119349223+sergioarturola@users.noreply.github.com>
2023-10-23 12:09:08 -0600

    diferentes formas de imprimir mensajes

---
+++ formas_imprimir.php
@@ -0,0 +1,17 @@
+<?php
+//Hay varias formas de dar salida en PHP
+//La mas comun
+
+echo "Hola a todo ";
+
+//otra forma que ademas nos muestra mas informacion 
+
+var_dump("hola");
+?>
+
+<?php
+//cuando por ejemplo se usa html y no queremos mezclar tantas etiquetas, simplemente usamos <?= codigo ?
+
+echo "Otro texto";
+
+?>
